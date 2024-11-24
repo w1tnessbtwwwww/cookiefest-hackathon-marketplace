aa89cdeec3029a501f3dbc39cc0ace90477aa554
Maksouni <max1205325@gmail.com>
2024-11-25 00:23:21 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './components/header/header'
+
+function App() {
+  
+
+  return (
+    <div className='wrapper'>
+      <Header />
+    </div>
+  )
+}
+
+export default App
