cba9f48c70a3dafd3758ce53b9c3fc286e985487
mark <samwellc@student.21-school.ru>
2024-11-27 17:16:52 +0700

    product page almost done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import Header from "./components/header/Header";
-import ProductPage from "./components/ProductPage";
+import ProductPage from './components/ProductPage';
 
 function App() {
   const product = {
-    imageUrl:
-      "https://basket-12.wbbasket.ru/vol1657/part165757/165757788/images/c516x688/1.webp",
+    imageUrl: "https://via.placeholder.com/600", // Главная фотография
     images: [
-      "https://via.placeholder.com/150",
-      "https://via.placeholder.com/150",
-      "https://via.placeholder.com/150",
+      "https://optim.tildacdn.com/tild3032-3364-4166-b538-646366303863/-/format/webp/2_29.jpg",
+      "https://optim.tildacdn.com/tild6162-3437-4461-a436-333562616235/-/format/webp/3_33.jpg",
+      "https://optim.tildacdn.com/tild3433-3061-4332-a239-353861666635/-/format/webp/4_37.jpg",
     ],
     title: "Футболка Ticle",
     price: "1620",
@@ -16,7 +15,8 @@ function App() {
     discount: "-67%",
     rating: 4.8,
     numReviews: 31601,
-    description: "Эта футболка изготовлена из премиального материала и идеально подходит для повседневного ношения.",
+    description:
+      "Эта футболка изготовлена из премиального материала и идеально подходит для повседневного ношения.",
     colors: [
       { name: "Бежевый", hex: "#F5F5DC" },
       { name: "Серый", hex: "#808080" },
@@ -24,21 +24,41 @@ function App() {
     ],
   };
 
+
+
   return (
     <div className="wrapper min-h-screen bg-gray-100 dark:bg-gray-900">
       <Header />
       <ProductPage
-        imageUrl={product.imageUrl}
-        images={product.images}
-        title={product.title}
-        price={product.price}
-        oldPrice={product.oldPrice}
-        discount={product.discount}
-        rating={product.rating}
-        numReviews={product.numReviews}
-        description={product.description}
-        colors={product.colors}
+        imagesByColor={{
+          Black: ['https://optim.tildacdn.com/tild3032-3364-4166-b538-646366303863/-/format/webp/2_29.jpg', 'https://optim.tildacdn.com/tild6162-3437-4461-a436-333562616235/-/format/webp/3_33.jpg', 'https://optim.tildacdn.com/tild3433-3061-4332-a239-353861666635/-/format/webp/4_37.jpg'],
+          White: ['https://optim.tildacdn.com/tild3439-3137-4263-a266-316232396330/-/format/webp/2_28.jpg', 'https://optim.tildacdn.com/tild6661-6262-4661-a261-666366396662/-/format/webp/4_36.jpg', 'https://optim.tildacdn.com/tild3639-6363-4738-b934-303362396132/-/format/webp/5_27.jpg'],
+          Beige: ['https://optim.tildacdn.com/tild3632-3030-4263-b038-336461666339/-/format/webp/2_30.jpg', 'https://optim.tildacdn.com/tild3166-3039-4333-b764-626133643433/-/format/webp/5_29.jpg', 'https://optim.tildacdn.com/tild3662-3761-4563-b434-613363386664/-/format/webp/4_38.jpg'],
+        }}
+        title="Футболка"
+        price="1620"
+        oldPrice="4960"
+        discount="-67%"
+        rating={4.8}
+        numReviews={31601}
+        description={`
+          Футболка оверсайз из плотного хлопка.
+          Состав и уход:
+          - Хлопок 100%
+          - Бережная стирка на 30°
+          - Отжим и сушка запрещена
+          - Не отбеливать
+      
+          На моделе размер: L
+          Параметры модели: рост: 180 | 108-76-110
+        `}
+        colors={[
+          { name: 'Black', hex: '#000000' },
+          { name: 'White', hex: '#FFFFFF' },
+          { name: 'Beige', hex: '#EED9C4' },
+        ]}
       />
+
     </div>
   );
 }
--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -1,102 +1,186 @@
-import React from "react";
+import React, { useState } from "react";
+import Swal from "sweetalert2";
 
 interface ProductPageProps {
-  imageUrl: string;
-  images: string[]; // Массив изображений для галереи
-  title: string;
-  price: string;
-  oldPrice?: string;
-  discount?: string;
-  rating: number;
-  numReviews: number;
-  description: string;
-  colors: { name: string; hex: string }[]; // Доступные цвета
+    imagesByColor: { [colorName: string]: string[] };
+    title: string;
+    price: string;
+    oldPrice?: string;
+    discount?: string;
+    rating: number;
+    numReviews: number;
+    description: string;
+    colors: { name: string; hex: string }[];
 }
 
 const ProductPage: React.FC<ProductPageProps> = ({
-  imageUrl,
-  images,
-  title,
-  price,
-  oldPrice,
-  discount,
-  rating,
-  numReviews,
-  description,
-  colors,
+    imagesByColor,
+    title,
+    price,
+    oldPrice,
+    discount,
+    rating,
+    numReviews,
+    description,
+    colors,
 }) => {
-  return (
-    <div className="wrapper min-h-screen bg-gray-100 dark:bg-gray-900 p-6">
-      <div className="container mx-auto flex flex-col lg:flex-row gap-10">
-        {/* Галерея изображений */}
-        <div className="flex-1">
-          <img
-            src={imageUrl}
-            alt="Product"
-            className="w-full h-[500px] object-cover rounded-lg"
-          />
-          <div className="mt-4 flex gap-2">
-            {images.map((img, index) => (
-              <img
-                key={index}
-                src={img}
-                alt={`Thumbnail ${index + 1}`}
-                className="w-16 h-16 object-cover rounded-lg cursor-pointer border border-gray-300 hover:border-purple-500"
-              />
-            ))}
-          </div>
-        </div>
+    const [activeColorIndex, setActiveColorIndex] = useState(0);
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    const [selectedSize, setSelectedSize] = useState<string | null>(null);
 
-        {/* Детали товара */}
-        <div className="flex-1 space-y-6">
-          {/* Название и рейтинг */}
-          <div>
-            <h1 className="text-3xl font-bold">{title}</h1>
-            <div className="flex items-center mt-2">
-              <span className="text-yellow-500 text-lg">{rating} ★</span>
-              <span className="ml-2 text-sm text-gray-500">({numReviews} отзывов)</span>
+    const sizes = ["S", "M", "L", "XL"];
+    const currentImages = imagesByColor[colors[activeColorIndex].name];
+
+    const handleImageChange = (index: number) => {
+        setCurrentImageIndex(index);
+    };
+
+    const handleColorChange = (index: number) => {
+        setActiveColorIndex(index);
+        setCurrentImageIndex(0);
+    };
+
+    const handleAddToCart = () => {
+        Swal.fire({
+            title: "Товар добавлен в корзину!",
+            text: "Теперь вы можете перейти в корзину или продолжить покупки.",
+            icon: "success",
+            position: "top-end",
+            timer: 3000,
+            showConfirmButton: false,
+            toast: true,
+        });
+    };
+
+    const handleNextImage = () => {
+        setCurrentImageIndex((prevIndex) =>
+            prevIndex < currentImages.length - 1 ? prevIndex + 1 : 0
+        );
+    };
+
+    const handlePrevImage = () => {
+        setCurrentImageIndex((prevIndex) =>
+            prevIndex > 0 ? prevIndex - 1 : currentImages.length - 1
+        );
+    };
+
+    return (
+        <div className="max-w-5xl mx-auto p-6 bg-white rounded-lg shadow-md">
+            <div className="flex">
+                {/* Левая часть */}
+                <div className="flex-1">
+                    <div className="relative w-full h-[500px] bg-gray-100 rounded-lg overflow-hidden">
+                        <img
+                            src={currentImages[currentImageIndex]}
+                            alt={`Product image ${currentImageIndex + 1}`}
+                            className="object-contain w-full h-full"
+                        />
+                        <button
+                            onClick={handlePrevImage}
+                            className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-gray-300 rounded-full p-2 hover:bg-gray-400"
+                        >
+                            ❮
+                        </button>
+                        <button
+                            onClick={handleNextImage}
+                            className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-gray-300 rounded-full p-2 hover:bg-gray-400"
+                        >
+                            ❯
+                        </button>
+                    </div>
+                    <div className="flex justify-center mt-4 space-x-2">
+                        {currentImages.map((image, index) => (
+                            <img
+                                key={index}
+                                src={image}
+                                alt={`Thumbnail ${index + 1}`}
+                                className={`w-16 h-16 object-cover rounded-lg cursor-pointer border-2 ${currentImageIndex === index
+                                    ? "border-[#5e5f9c]"
+                                    : "border-transparent"
+                                    }`}
+                                onClick={() => handleImageChange(index)}
+                            />
+                        ))}
+                    </div>
+                </div>
+
+                {/* Правая часть */}
+                <div className="flex-1 ml-8">
+                    <h1 className="text-2xl font-bold text-gray-800">{title}</h1>
+                    <div className="mt-2 flex items-center space-x-4">
+                        <span className="text-xl font-bold text-gray-900">{price} ₽</span>
+                        {oldPrice && (
+                            <span className="text-sm text-gray-500 line-through">
+                                {oldPrice} ₽
+                            </span>
+                        )}
+                        {discount && (
+                            <span className="text-sm bg-red-600 text-white px-2 py-1 rounded">
+                                {discount}
+                            </span>
+                        )}
+                    </div>
+                    <div className="mt-2 flex items-center">
+                        <span className="text-yellow-500 font-medium">{rating}</span>
+                        <span className="ml-2 text-sm text-gray-500">
+                            ({numReviews} отзывов)
+                        </span>
+                    </div>
+                    <p className="mt-2 text-sm text-gray-600 whitespace-pre-line">
+                        {description}
+                    </p>
+
+                    {/* Выбор цвета */}
+                    <div className="mt-4">
+                        <h3 className="text-sm font-medium text-gray-700">Цвет:</h3>
+                        <div className="mt-2 flex space-x-4">
+                            {colors.map((color, index) => (
+                                <button
+                                    key={color.name}
+                                    onClick={() => handleColorChange(index)}
+                                    className={`w-8 h-8 rounded-full border-2 focus:outline-none ${activeColorIndex === index
+                                            ? "border-[#5e5f9c]"
+                                            : "border-transparent"
+                                        } ${color.name === "White" ? "shadow-md shadow-gray-500" : ""}`}
+                                    style={{ backgroundColor: color.hex }}
+                                ></button>
+                            ))}
+                        </div>
+                    </div>
+
+
+                    {/* Выбор размера */}
+                    <div className="mt-6">
+                        <h3 className="text-sm font-medium text-gray-700">Размер:</h3>
+                        <div className="mt-2 flex space-x-4">
+                            {sizes.map((size) => (
+                                <button
+                                    key={size}
+                                    onClick={() => setSelectedSize(size)}
+                                    className={`w-8 h-8 flex items-center justify-center rounded-full border-2 text-sm font-medium ${selectedSize === size
+                                        ? "border-[#5e5f9c] bg-[#5e5f9c] text-white"
+                                        : "border-transparent text-gray-700"
+                                        }`}
+                                >
+                                    {size}
+                                </button>
+                            ))}
+                        </div>
+                    </div>
+
+
+
+                    {/* Кнопка добавления в корзину */}
+                    <button
+                        className="mt-8 w-full bg-[#5e5f9c] hover:bg-[#4a4b7a] text-white text-sm font-medium py-2 rounded-lg"
+                        onClick={handleAddToCart}
+                    >
+                        Добавить в корзину
+                    </button>
+                </div>
             </div>
-          </div>
-
-          {/* Цена и скидка */}
-          <div className="flex items-center">
-            <span className="text-3xl font-bold text-gray-900">{price} ₽</span>
-            {oldPrice && (
-              <span className="ml-4 text-lg text-gray-500 line-through">{oldPrice} ₽</span>
-            )}
-            {discount && (
-              <span className="ml-4 bg-red-600 text-white text-sm font-medium px-2 py-0.5 rounded">
-                {discount}
-              </span>
-            )}
-          </div>
-
-          {/* Описание */}
-          <p className="text-gray-700">{description}</p>
-
-          {/* Выбор цвета */}
-          <div className="space-y-2">
-            <h3 className="text-lg font-semibold">Выберите цвет:</h3>
-            <div className="flex gap-2">
-              {colors.map((color) => (
-                <div
-                  key={color.name}
-                  className="w-8 h-8 rounded-full cursor-pointer border border-gray-300"
-                  style={{ backgroundColor: color.hex }}
-                  title={color.name}
-                ></div>
-              ))}
-            </div>
-          </div>
-
-          {/* Кнопка */}
-          <button className="w-full bg-purple-600 hover:bg-purple-700 text-white text-lg font-medium py-3 rounded-lg">
-            Добавить в корзину
-          </button>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ProductPage;
