f152a71561e4ec46b2093132aa57f47ff9a80336
Ray <tim.mihalev2015@ya.ru>
2024-12-01 05:27:09 +0700

    indexer

---
+++ back/indexer/src/main/java/ru/raytrace/indexer/service/IndexInitializatorService.java
@@ -0,0 +1,35 @@
+package ru.raytrace.indexer.service;
+
+import okhttp3.*;
+import org.springframework.stereotype.Service;
+import java.io.File;
+import java.io.IOException;
+
+@Service
+public class IndexInitializatorService {
+
+    private final OkHttpClient client;
+    public static final MediaType JSON = MediaType.get("application/json");
+
+    public IndexInitializatorService(OkHttpClient client) {
+        this.client = client;
+        initialize();
+    }
+
+    public void initialize() {
+        File json = new File(getClass().getClassLoader().getResource("indexinit.json").getFile());
+
+        RequestBody requestBody = RequestBody.create(json, JSON);
+        Request request = new Request.Builder()
+                .url("https://localhost:9200/products")
+                .put(requestBody)
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Authorization", "ApiKey ZHc1ZWZaTUI4M0VKUEtHT2tUTUQ6SmY1UnNhSHdTN0s2YUwtemJ2d1ZfZw==")
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            System.out.println(response.body().string());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
